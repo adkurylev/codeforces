1e5fbd71ba85a6eea07fd87c4b1a30b47d9e4133
adkurylev <adkurylev@gmail.com>
2019-07-25 15:36:20 +0300

    Olympiad done

---
+++ Olympiad/cmake-build-debug/CMakeFiles/Olympiad.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Olympiad.dir/main.cpp.obj"
+  "Olympiad.pdb"
+  "Olympiad.exe"
+  "Olympiad.exe.manifest"
+  "Olympiad.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Olympiad.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Olympiad/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    int * array;
+    int n;
+    cin >> n;
+    array = new int[n];
+    set<int> markSet;
+    for (int i = 0; i < n; ++i) {
+        cin >> array[i];
+        markSet.insert(array[i]);
+    }
+
+    int out = markSet.count(0) ? markSet.size() - 1 : markSet.size();
+
+    cout << out;
+
+    return 0;
+}
