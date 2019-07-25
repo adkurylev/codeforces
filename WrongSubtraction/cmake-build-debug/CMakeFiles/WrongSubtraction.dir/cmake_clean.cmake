033ed69928c4ee1ae2811d5870024e90b759cd53
adkurylev <adkurylev@gmail.com>
2019-07-25 13:51:39 +0300

    WrongSubtraction done

---
+++ WrongSubtraction/cmake-build-debug/CMakeFiles/WrongSubtraction.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WrongSubtraction.dir/main.cpp.obj"
+  "WrongSubtraction.pdb"
+  "WrongSubtraction.exe"
+  "WrongSubtraction.exe.manifest"
+  "WrongSubtraction.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WrongSubtraction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
