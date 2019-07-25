18d6016747997ae528bd781744c0980b7b287009
adkurylev <adkurylev@gmail.com>
2019-07-25 14:24:05 +0300

    OlderBrother done

---
+++ OlderBrother/cmake-build-debug/CMakeFiles/OlderBrother.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OlderBrother.dir/main.cpp.obj"
+  "OlderBrother.pdb"
+  "OlderBrother.exe"
+  "OlderBrother.exe.manifest"
+  "OlderBrother.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OlderBrother.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ OlderBrother/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, n = 0;
+    cin >> a >> b;
+
+    while(a <= b){
+        a *= 3;
+        b *= 2;
+        n++;
+    }
+
+    cout << n;
+
+    return 0;
+}
