e32fee62508416b20d5475e9dba7cfa073e15814
adkurylev <adkurylev@gmail.com>
2019-07-26 15:44:20 +0300

    OlderBrother done

---
+++ BinarySearch/cmake-build-debug/CMakeFiles/BinarySearch.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BinarySearch.dir/main.cpp.obj"
+  "BinarySearch.pdb"
+  "BinarySearch.exe"
+  "BinarySearch.exe.manifest"
+  "BinarySearch.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BinarySearch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
